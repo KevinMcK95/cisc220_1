3183c6cc83764a318e9dafdc6327403a7c15df46
KevinMcK95 <KevinMcK95@shaw.ca>
2016-10-05 19:14:56 -0400

    calculator works and is finished

--- calculator.sh
+++ calculator.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 
+RESULT=a
+NUMTURN=1
 NUM1=a
-RESULT=0
+OLDNUM=$NUM1
+OPERATOR=0
+END=:q
 
-while true ; do
-
-	case $NUM1 in
-		*[!0-9]*) read -p "Please insert a number: " NUM1 ;;
-		*) break ;;
-	esac
+while [ "$NUM1" != "$END" ] && [ "$OPERATOR" != "$END" ] ; do
+	if (( $NUMTURN==1  )) ; then
+		read -p "Please insert a number: " NUM1 
+		if (( ${#NUM1}<1 )) ; then
+			echo -n "Error! "
+			continue
+		elif [ "$NUM1" = ":q" ] ; then
+			continue
+		fi
+		case $NUM1 in
+			*[!0-9]*) echo -n "Error! " ;;
+			*) NUMTURN=0
+			   if [ "$OPERATOR" != "0" ] ; then
+				if [ "$RESULT" = "a" ] ; then
+					RESULT=$(( $OLDNUM${OPERATOR}$NUM1 ))
+				else
+					RESULT=$(( $RESULT${OPERATOR}$NUM1 ))
+				fi
+				echo Result so far is: $RESULT
+			   fi
+			   OLDNUM=$NUM1 ;;
+		esac
+	else
+		read -p "Please insert an operator: " OPERATOR 
+		if (( ${#OPERATOR}<1 )) ; then
+			echo -n "Error! "
+			continue
+		elif [ "$OPERATOR" = ":q" ] ; then
+			continue
+		fi
+		if [ "$OPERATOR" = "+" ] || [ "$OPERATOR" = "-" ] || [ "$OPERATOR" = "/" ] || [ "$OPERATOR" = "*" ] ; then
+			NUMTURN=1
+		else
+			echo -n "Error! "
+		fi
+	fi
 done
-echo $NUM1
+echo Thanks for using my calculator

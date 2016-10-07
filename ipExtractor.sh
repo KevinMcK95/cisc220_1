d4203653eff500e7b4aa63784006af53bfd7b8b3
KevinMcK95 <KevinMcK95@shaw.ca>
2016-10-07 17:30:41 -0400

    ipExtractor.sh added and is working. Boom shaka laka.

---
+++ ipExtractor.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+fullList=$(ifconfig)
+fullList=( ${fullList[*]}  )
+
+
+declare -a ip
+ip=( )
+ipTitle="inet addr:"
+inet="inet"
+addr="addr:"
+
+for i in $(seq 0 ${#fullList[*]}) ; do
+	string=${fullList[$i]}
+	nextIndex=$(($i+1))
+	nextString=${fullList[$nextIndex]}
+	testString="$string $nextString"
+	if [ "${testString:0:10}" = "$ipTitle" ] ; then
+		ip+=( ${testString:10:${#nextString}} )
+	fi
+done
+
+for add in ${ip[*]} ; do
+	echo -n "$add "
+	if [[ $add == 127* ]] ; then
+		echo Local IP
+	elif [[ $add == 10* ]] ; then
+		echo Private IP
+	elif [[ $add == 192.168* ]] ; then
+		echo Private IP
+	else
+		echo Public IP
+	fi
+done

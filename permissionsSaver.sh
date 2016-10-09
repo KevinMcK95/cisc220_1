2d2ab046b48143d16fa8a5c0d1e3912668a15154
KevinMcK95 <KevinMcK95@shaw.ca>
2016-10-08 20:32:18 -0400

    permissionsSavers.sh is close to working except for booleans

--- permissionsSaver.sh
+++ permissionsSaver.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+fullList=$(ls -Rl $PWD)
+fullList=( ${fullList[*]} )
+
+for i in $(seq 0 $((${#fullList[*]}-8))) ; do
+	string=${fullList[$i]}
+	name=${fullList[$(($i+8))]}
+	if (( ${#string}==10 )) && [ "${string[0]}" = "-" ] ; then
+	if  [[ $string=="*w*" || $string=="*x*" || $string=="*r*" || $string=="*-*"  ]] ; then
+		echo $string $name
+	fi
+	fi
+done
 

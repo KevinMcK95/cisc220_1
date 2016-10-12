bc7b9dcc617954df3980070efbb885bb6ba6e474
KevinMcK95 <KevinMcK95@shaw.ca>
2016-10-12 08:46:50 -0400

    permissionsSaver.sh now fully operational!

--- permissionsSaver.sh
+++ permissionsSaver.sh
@@ -3,13 +3,36 @@
 fullList=$(ls -Rl $PWD)
 fullList=( ${fullList[*]} )
 
+count=1
+if [ -f savedPermissions ] ; then
+	while [ -f savedPermissions$count ] ; do
+		count=$(($count+1))
+	done
+	saveFile=savedPermissions$count
+else
+	saveFile=savedPermissions
+fi
+
 for i in $(seq 0 $((${#fullList[*]}-8))) ; do
-	string=${fullList[$i]}
-	name=${fullList[$(($i+8))]}
-	if (( ${#string}==10 )) && [ "${string[0]}" = "-" ] ; then
-	if  [[ $string=="*w*" || $string=="*x*" || $string=="*r*" || $string=="*-*"  ]] ; then
-		echo $string $name
-	fi
+	string="${fullList[$i]}"
+	name="${fullList[$(($i+8))]}"
+	firstChar=${string:0:1}
+	if (( ${#string}==10 )) && [ "$firstChar" = "-" ] ; then
+	for j in $(seq 1 $((${#string}-1)) ) ; do
+		substring=${string:$(($j)):1}
+		
+		if (( $(($j%3))==1 )) && [ "$substring" != "r" ] && [ "$substring" != "-" ] ; then
+			break
+		elif (( $(($j%3))==2 )) && [ "$substring" != "w" ] && [ "$substring" != "-" ] ; then
+			break
+		elif (( $(($j%3))==0 )) && [ "$substring" != "x" ] && [ "$substring" != "-" ] ; then
+			break
+		fi
+		if (( $j==$((${#string}-1)) )); then #reached the end without breaking
+			echo $string $name >> $saveFile
+		fi
+	done
 	fi
 done
 
+echo $saveFile created!
